50ed4582bd0bab528aedc4fccdd40a68e1b90938
zayed528 <zayed528ali@gmail.com>
2026-02-07 16:39:05 +0000

    Pathway

--- Untitled File 2026-02-07 11_26_09.py
+++ Untitled File 2026-02-07 11_26_09.py
@@ -1,1 +1,8 @@
 #TEest
+df = spark.read.csv(
+    "/Volumes/workspace/default/osu-energy-analysis/DATA I-O 2026 Advanced Datasets/advanced_core/advanced_core/building_metadata.csv",
+    header=True
+)
+
+display(df)
+
